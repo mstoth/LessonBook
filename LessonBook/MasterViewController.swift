5a7113593c4529f834d2252b15d553f527fbf611
Michael Toth <michael@virtualpianist.com>
2019-01-22 18:27:04 -0500

    first commit, templet but with Student class.

--- LessonBook/MasterViewController.swift
+++ LessonBook/MasterViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import CloudKit
 
 class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
 
@@ -36,10 +37,11 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     @objc
     func insertNewObject(_ sender: Any) {
         let context = self.fetchedResultsController.managedObjectContext
-        let newEvent = Event(context: context)
-             
+        //let newEvent = Event(context: context)
+        let newStudent = Student(context: context)
         // If appropriate, configure the new managed object.
-        newEvent.timestamp = Date()
+        newStudent.firstName = "New"
+        newStudent.lastName = "Student"
 
         // Save the context.
         do {
@@ -79,8 +81,9 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let event = fetchedResultsController.object(at: indexPath)
-        configureCell(cell, withEvent: event)
+        //let event = fetchedResultsController.object(at: indexPath)
+        let student = fetchedResultsController.object(at: indexPath)
+        configureCell(cell, withStudent: student)
         return cell
     }
 
@@ -105,24 +108,24 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         }
     }
 
-    func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
-        cell.textLabel!.text = event.timestamp!.description
+    func configureCell(_ cell: UITableViewCell, withStudent student: Student) {
+        cell.textLabel!.text = student.firstName! + " " + student.lastName!
     }
 
     // MARK: - Fetched results controller
 
-    var fetchedResultsController: NSFetchedResultsController<Event> {
+    var fetchedResultsController: NSFetchedResultsController<Student> {
         if _fetchedResultsController != nil {
             return _fetchedResultsController!
         }
         
-        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
+        let fetchRequest: NSFetchRequest<Student> = Student.fetchRequest()
         
         // Set the batch size to a suitable number.
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "lastName", ascending: false)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
@@ -143,7 +146,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         
         return _fetchedResultsController!
     }    
-    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
+    var _fetchedResultsController: NSFetchedResultsController<Student>? = nil
 
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
@@ -167,9 +170,9 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
             case .delete:
                 tableView.deleteRows(at: [indexPath!], with: .fade)
             case .update:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
+                configureCell(tableView.cellForRow(at: indexPath!)!, withStudent: anObject as! Student)
             case .move:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
+                configureCell(tableView.cellForRow(at: indexPath!)!, withStudent: anObject as! Student)
                 tableView.moveRow(at: indexPath!, to: newIndexPath!)
         }
     }
